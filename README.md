adede804f10cf4be16bbaec62a23a24e541efe23
Redhwan Nacef <nacefredhwan@gmail.com>
2022-07-27 13:44:03 +0100

    Initialise repository with script

---
+++ README.md
@@ -0,0 +1,12 @@
+# Elgato Light
+
+This is a small Deno script that allows incrementing/decrementing of the Elgato Key Light.
+
+In my setup I have one light. I have bound this script to the brightness modifiers on my Keychron keyboard.
+
+## Usage:
+
+```shell
+  deno run -A index.ts increment # increments brightness by 10
+  deno run -A index.ts decrement # increments brightness by 10
+```
---
+++ index.ts
@@ -0,0 +1,30 @@
+const action = Deno.args[0] as Action;
+const url = "http://192.168.1.168:9123/elgato/lights";
+
+const status: LightsStatus = await fetch(url).then((response) =>
+  response.json()
+);
+
+const currentBrightness = status.lights[0].brightness;
+const brightness =
+  action === "increment"
+    ? Math.min(100, currentBrightness + 10)
+    : Math.max(0, currentBrightness - 10);
+
+await fetch(url, {
+  method: "PUT",
+  body: JSON.stringify({ lights: [{ brightness }] }),
+});
+
+type Action = "increment" | "decrement";
+
+type LightStatus = {
+  on: number;
+  brightness: number;
+  temperature: number;
+};
+
+type LightsStatus = {
+  numberOfLights: number;
+  lights: LightStatus[];
+};
